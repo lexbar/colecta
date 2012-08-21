8cbff81e17e7e9161d0e7d034e2e51e0e7a7dc25
Alex <zumbenet@gmail.com>
2012-08-21 17:23:30 +0200

    Create Event

--- src/Colecta/ActivityBundle/Controller/EventController.php
+++ src/Colecta/ActivityBundle/Controller/EventController.php
@@ -3,6 +3,8 @@
 namespace Colecta\ActivityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Colecta\ActivityBundle\Entity\Event;
 
 
 class EventController extends Controller
@@ -11,10 +13,10 @@ class EventController extends Controller
     {
         $em = $this->getDoctrine()->getEntityManager();
         
-        //Get ALL the items that are not drafts
         $items = $em->getRepository('ColectaActivityBundle:Event')->findBy(array('draft'=>0), array('date'=>'DESC'),10,0);
+        $categories = $em->getRepository('ColectaItemBundle:Category')->findAll();
         
-        return $this->render('ColectaActivityBundle:Event:index.html.twig', array('items' => $items));
+        return $this->render('ColectaActivityBundle:Event:index.html.twig', array('items' => $items, 'categories' => $categories));
     }
     public function viewAction($slug)
     {
@@ -23,5 +25,61 @@ class EventController extends Controller
         $item = $em->getRepository('ColectaActivityBundle:Event')->findOneBySlug($slug);
         
         return $this->render('ColectaActivityBundle:Event:full.html.twig', array('item' => $item));
+    }
+    public function createAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getEntityManager();
+        $request = $this->get('request')->request;
+        
+        $category = $em->getRepository('ColectaItemBundle:Category')->findOneById($request->get('category'));
+    
+        if(!$user) 
+        {
+            $this->get('session')->setFlash('error', 'Error, debes iniciar sesion');
+        }
+        elseif(!$request->get('text'))
+        {
+            $this->get('session')->setFlash('error', 'No has escrito ningun texto');
+        }
+        elseif(!$category)
+        {
+            $this->get('session')->setFlash('error', 'No existe la categoria');
+        }
+        else
+        {
+            $event = new Event();
+            $event->setCategory($category);
+            $event->setAuthor($user);
+            $event->setName($request->get('name'));
+            $event->setSlug(str_replace(' ', '_', strtolower($request->get('name'))));
+            $event->setSummary( substr($request->get('text'),0,255) );
+            $event->setTagwords( substr($request->get('text'),255,255) );
+            $event->setDate(new \DateTime('now'));
+            $event->setAllowComments(true);
+            $event->setDraft(false);
+            $event->setActivity(null);
+            $event->setDateini(new \DateTime(trim($request->get('dateini')).' '.$request->get('dateinihour').':'.$request->get('dateiniminute')));
+            $event->setDateend(new \DateTime(trim($request->get('dateend')).' '.$request->get('dateendhour').':'.$request->get('dateendminute')));
+            $event->setShowhours(false);
+            $event->setDescription($request->get('description'));
+            $event->setDistance($request->get('distance'));
+            $event->setUphill($request->get('uphill'));
+            $event->setDownhill($request->get('downhill'));
+            $event->setDifficulty($request->get('difficulty'));
+            $event->setStatus('');
+            
+            $em->persist($event); 
+            $em->flush();
+        }
+        
+        $referer = $this->get('request')->headers->get('referer');
+        
+        if(empty($referer))
+        {
+            $referer = $this->generateUrl('ColectaEventIndex');
+        }
+        
+        return new RedirectResponse($referer);
     }
 }
--- src/Colecta/ItemBundle/Controller/PostController.php
+++ src/Colecta/ItemBundle/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace Colecta\ItemBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Colecta\ItemBundle\Entity\Post;
 
 class PostController extends Controller
@@ -12,8 +13,9 @@ class PostController extends Controller
         $em = $this->getDoctrine()->getEntityManager();
         
         $items = $em->getRepository('ColectaItemBundle:Post')->findBy(array('draft'=>0), array('date'=>'DESC'),10,0);
+        $categories = $em->getRepository('ColectaItemBundle:Category')->findAll();
         
-        return $this->render('ColectaItemBundle:Post:index.html.twig', array('items' => $items));
+        return $this->render('ColectaItemBundle:Post:index.html.twig', array('items' => $items, 'categories' => $categories));
     }
     public function viewAction($slug)
     {
@@ -22,5 +24,53 @@ class PostController extends Controller
         $item = $em->getRepository('ColectaItemBundle:Post')->findOneBySlug($slug);
         
         return $this->render('ColectaItemBundle:Post:full.html.twig', array('item' => $item));
+    }
+    public function createAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getEntityManager();
+        $request = $this->get('request')->request;
+        
+        $category = $em->getRepository('ColectaItemBundle:Category')->findOneById($request->get('category'));
+    
+        if(!$user) 
+        {
+            $this->get('session')->setFlash('error', 'Error, debes iniciar sesion');
+        }
+        elseif(!$request->get('text'))
+        {
+            $this->get('session')->setFlash('error', 'No has escrito ningun texto');
+        }
+        elseif(!$category)
+        {
+            $this->get('session')->setFlash('error', 'No existe la categoria');
+        }
+        else
+        {
+            $post = new Post();
+            $post->setCategory($category);
+            $post->setAuthor($user);
+            $post->setName($request->get('name'));
+            $post->setSlug(str_replace(' ', '_', strtolower($request->get('name'))));
+            $post->setSummary( substr($request->get('text'),0,255) );
+            $post->setTagwords( substr($request->get('text'),255,255) );
+            $post->setDate(new \DateTime('now'));
+            $post->setAllowComments(true);
+            $post->setDraft(false);
+            $post->setText($request->get('text'));
+            
+            
+            $em->persist($post); 
+            $em->flush();
+        }
+        
+        $referer = $this->get('request')->headers->get('referer');
+        
+        if(empty($referer))
+        {
+            $referer = $this->generateUrl('ColectaPostIndex');
+        }
+        
+        return new RedirectResponse($referer);
     }
 }
