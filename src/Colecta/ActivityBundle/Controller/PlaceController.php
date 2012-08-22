6a5797640dec3dd5f2f42897489f2401e3b57b0e
Alex <zumbenet@gmail.com>
2012-08-22 02:14:23 +0200

    Create Contest

--- src/Colecta/ActivityBundle/Controller/PlaceController.php
+++ src/Colecta/ActivityBundle/Controller/PlaceController.php
@@ -3,7 +3,8 @@
 namespace Colecta\ActivityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Colecta\ActivityBundle\Entity\Place;
 
 class PlaceController extends Controller
 {
@@ -13,8 +14,9 @@ class PlaceController extends Controller
         
         //Get ALL the items that are not drafts
         $items = $em->getRepository('ColectaActivityBundle:Place')->findBy(array('draft'=>0), array('date'=>'DESC'),10,0);
-        
-        return $this->render('ColectaActivityBundle:Place:index.html.twig', array('items' => $items));
+        $categories = $em->getRepository('ColectaItemBundle:Category')->findAll();
+
+        return $this->render('ColectaActivityBundle:Place:index.html.twig', array('items' => $items, 'categories' => $categories));
     }
     public function viewAction($slug)
     {
@@ -24,4 +26,54 @@ class PlaceController extends Controller
         
         return $this->render('ColectaActivityBundle:Place:full.html.twig', array('item' => $item));
     }
+    public function createAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getEntityManager();
+        $request = $this->get('request')->request;
+        
+        $category = $em->getRepository('ColectaItemBundle:Category')->findOneById($request->get('category'));
+    
+        if(!$user) 
+        {
+            $this->get('session')->setFlash('error', 'Error, debes iniciar sesion');
+        }
+        elseif(!$request->get('name'))
+        {
+            $this->get('session')->setFlash('error', 'No has escrito ningun nombre');
+        }
+        elseif(!$category)
+        {
+            $this->get('session')->setFlash('error', 'No existe la categoria');
+        }
+        else
+        {
+            $place= new Place();
+            $place->setCategory($category);
+            $place->setAuthor($user);
+            $place->setName($request->get('name'));
+            $place->setSlug(str_replace(' ', '_', strtolower($request->get('name'))));
+            $place->setSummary( substr($request->get('description'),0,255) );
+            $place->setTagwords( substr($request->get('description'),255,255) );
+            $place->setDate(new \DateTime('now'));
+            $place->setAllowComments(true);
+            $place->setDraft(false);
+            $place->setDescription($request->get('description'));
+            $place->setLatitude($request->get('latitude'));
+            $place->setLongitude($request->get('longitude'));
+            
+            $em->persist($place); 
+            $em->flush();
+        }
+        
+        $referer = $this->get('request')->headers->get('referer');
+        
+        if(empty($referer))
+        {
+            $referer = $this->generateUrl('ColectaPlaceIndex');
+        }
+        
+        return new RedirectResponse($referer);
+    }
+
 }
---
+++ src/Colecta/ColectiveBundle/Controller/ContestController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Colecta\ColectiveBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Colecta\ColectiveBundle\Entity\Contest;
+use Colecta\ColectiveBundle\Entity\ContestWinner;
+
+class ContestController extends Controller
+{
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        //Get ALL the items that are not drafts
+        $items = $em->getRepository('ColectaColectiveBundle:Contest')->findBy(array('draft'=>0), array('date'=>'DESC'),10,0);
+        $categories = $em->getRepository('ColectaItemBundle:Category')->findAll();
+
+        return $this->render('ColectaColectiveBundle:Contest:index.html.twig', array('items' => $items, 'categories' => $categories));
+    }
+    public function viewAction($slug)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $item = $em->getRepository('ColectaColectiveBundle:Contest')->findOneBySlug($slug);
+        
+        return $this->render('ColectaColectiveBundle:Contest:full.html.twig', array('item' => $item));
+    }
+    public function createAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getEntityManager();
+        $request = $this->get('request')->request;
+        
+        $category = $em->getRepository('ColectaItemBundle:Category')->findOneById($request->get('category'));
+    
+        if(!$user) 
+        {
+            $this->get('session')->setFlash('error', 'Error, debes iniciar sesion');
+        }
+        elseif(!$request->get('name'))
+        {
+            $this->get('session')->setFlash('error', 'No has escrito ningun nombre');
+        }
+        elseif(!$category)
+        {
+            $this->get('session')->setFlash('error', 'No existe la categoria');
+        }
+        else
+        {
+            $contest= new Contest();
+            $contest->setCategory($category);
+            $contest->setAuthor($user);
+            $contest->setName($request->get('name'));
+            $contest->setSlug(str_replace(' ', '_', strtolower($request->get('name'))));
+            $contest->setSummary( substr($request->get('description'),0,255) );
+            $contest->setTagwords( substr($request->get('description'),255,255) );
+            $contest->setDate(new \DateTime('now'));
+            $contest->setAllowComments(true);
+            $contest->setDraft(false);
+            $contest->setDescription($request->get('description'));
+            $contest->setIniDate(new \DateTime('now'));
+            $contest->setEndDate(new \DateTime('now'));
+            $contest->setItemTypes('');
+            
+            $n = 0;
+            
+            while($request->get('position'.$n))
+            {
+                $victory = new ContestWinner();
+                $victory->setPosition($request->get('position'.$n));
+                $victory->setText($request->get('position'.$n.'text'));
+                $victory->setContest($contest);
+                $victory->setItem(null);
+                $victory->setUser(null);
+                
+                $contest->addContestWinner($victory);
+                $em->persist($victory); 
+                
+                $n++;
+            }
+            
+            $em->persist($contest); 
+            $em->flush();
+        }
+        
+        $referer = $this->get('request')->headers->get('referer');
+        
+        if(empty($referer))
+        {
+            $referer = $this->generateUrl('ColectaContestIndex');
+        }
+        
+        return new RedirectResponse($referer);
+    }
+}
