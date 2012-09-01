de3d9de5e3c4cad82a1c05099e22716616c72b77
Alex <zumbenet@gmail.com>
2012-09-01 16:53:45 +0200

    Remove of FOLLOW ALLOCATION on Curl

--- src/Colecta/ActivityBundle/Controller/RouteController.php
+++ src/Colecta/ActivityBundle/Controller/RouteController.php
@@ -580,7 +580,6 @@ function getContent($url)
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url );
 	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); //Needed because redirection is used on the app
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 7);
