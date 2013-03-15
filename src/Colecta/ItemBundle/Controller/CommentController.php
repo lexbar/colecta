bcc2aefa835e5d009f9f094727bc3ec93ac4323f
Alex <zumbenet@gmail.com>
2013-03-15 18:16:48 +0100

    Notificarions on comments

--- src/Colecta/ItemBundle/Controller/CommentController.php
+++ src/Colecta/ItemBundle/Controller/CommentController.php
@@ -4,7 +4,7 @@ namespace Colecta\ItemBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-
+use Colecta\UserBundle\Entity\Notification;
 use Colecta\ItemBundle\Entity\Comment;
 
 class CommentController extends Controller
@@ -18,7 +18,7 @@ class CommentController extends Controller
         
         $item = $em->getRepository('ColectaItemBundle:Item')->findOneBySlug($slug);
     
-        if(!$item || !$user || !$post->get('comment')) 
+        if(!$item || $user == 'anon.' || !$post->get('comment')) 
         {
             $this->get('session')->setFlash('error', 'Error escribiendo el comentario');
         }
@@ -31,6 +31,42 @@ class CommentController extends Controller
             $comment->setDate(new \DateTime('now'));
             
             $em->persist($comment); 
+            
+            $notifications = array();
+            
+            if($user != $item->getauthor())
+            {
+                //Notification to the owner
+                $notification = new Notification();
+                $notification->setUser($item->getAuthor());
+                $notification->setDismiss(0);
+                $notification->setDate(new \DateTime('now'));
+                $notification->setText($user->getName().' ha escrito un comentario en <a href="'.$this->generateUrl('ColectaEventView', array('slug'=>$item->getSlug())).'">'.$item->getName().'</a>');
+                $em->persist($notification); 
+                
+                $notifications[] = $item->getAuthor()->getId();
+            }
+            
+            $comments = $item->getComments();
+            
+            foreach($comments as $c) 
+            {
+                if(!in_array($c->getUser()->getId(), $notifications)) //if he has not already received a notification..
+                {
+                    //Notification to a subscriber
+                    $notification = new Notification();
+                    $notification->setUser($c->getUser());
+                    $notification->setDismiss(0);
+                    $notification->setDate(new \DateTime('now'));
+                    $notification->setText($user->getName().' ha contestado en <a href="'.$this->generateUrl('ColectaEventView', array('slug'=>$item->getSlug())).'">'.$item->getName().'</a>');
+                    $em->persist($notification); 
+                    
+                    $notifications[] = $c->getUser()->getId();
+                }
+            }
+                            
+            
+                    
             $em->flush();
         }
         
