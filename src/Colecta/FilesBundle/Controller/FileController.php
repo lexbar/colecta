628269157dd862d4993b53683a0c6b4f74fb772c
Alex <zumbenet@gmail.com>
2013-05-27 01:32:32 +0200

    Auto rotate images

--- src/Colecta/FilesBundle/Controller/FileController.php
+++ src/Colecta/FilesBundle/Controller/FileController.php
@@ -487,6 +487,7 @@ class FileController extends Controller
             }
             
             $image = new \Imagick($item->getAbsolutePath());
+            autoRotateImage($image);
             $image->cropThumbnailImage($width, $height);
             $image->setImagePage(0, 0, 0, 0);
             $image->normalizeImage();
@@ -543,6 +544,7 @@ class FileController extends Controller
             }
             
             $image = new \Imagick($item->getAbsolutePath());
+            autoRotateImage($image);
             $image->setImageResolution(72,72); 
             $image->scaleImage($width, $height, true);
             $image->setImagePage(0, 0, 0, 0);
@@ -584,3 +586,24 @@ class FileController extends Controller
         return $response;
     }
 }
+
+function autoRotateImage($image) {
+    $orientation = $image->getImageOrientation();
+
+    switch($orientation) {
+        case imagick::ORIENTATION_BOTTOMRIGHT:
+            $image->rotateimage("#000", 180); // rotate 180 degrees
+        break;
+
+        case imagick::ORIENTATION_RIGHTTOP:
+            $image->rotateimage("#000", 90); // rotate 90 degrees CW
+        break;
+
+        case imagick::ORIENTATION_LEFTBOTTOM:
+            $image->rotateimage("#000", -90); // rotate 90 degrees CCW
+        break;
+    }
+
+    // Now that it's auto-rotated, make sure the EXIF data is correct in case the EXIF gets saved with the image!
+    $image->setImageOrientation(imagick::ORIENTATION_TOPLEFT);
+} 
