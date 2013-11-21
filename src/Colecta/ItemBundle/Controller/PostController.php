1f433a70fb38dd8c30455a7d6b0c73167bb6b496
Alex <zumbenet@gmail.com>
2013-11-21 06:28:30 +0100

    Bug fix

--- src/Colecta/ItemBundle/Controller/PostController.php
+++ src/Colecta/ItemBundle/Controller/PostController.php
@@ -343,7 +343,6 @@ function getContent($url) {
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url );
 	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); //Needed because redirection is used on the app
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 15);
