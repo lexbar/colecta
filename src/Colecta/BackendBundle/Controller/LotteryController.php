b74063e382e474d6daaa0675f8d12e37c71f7018
Alex <zumbenet@gmail.com>
2013-12-17 19:00:03 +0100

    Lottery backend improvements

--- src/Colecta/BackendBundle/Controller/LotteryController.php
+++ src/Colecta/BackendBundle/Controller/LotteryController.php
@@ -28,6 +28,12 @@ class LotteryController extends Controller
             $userShreds[$s->getUser()->getId()][] = $s;
         }
         
+        usort($userShreds, function($a, $b)
+            {
+                return strcmp($a[0]->getUser()->getName(), $b[0]->getUser()->getName());
+            }
+        );
+        
         return $this->render('ColectaBackendBundle:Lottery:index.html.twig', array('lotteryCampaign'=>$campaign, 'lotteryShreds'=>$userShreds));
     }
     
@@ -52,7 +58,6 @@ class LotteryController extends Controller
     public function addShredAction()
     {
         $user = $this->get('security.context')->getToken()->getUser();
-        $request = $this->get('request')->request;
         
         if($user == 'anon.' || !$user->getRole()->getSiteConfig())
         {
@@ -60,10 +65,20 @@ class LotteryController extends Controller
             return new RedirectResponse($login);
         }
         
+        $request = $this->get('request')->request;
+        
+        $start = intval($request->get('start'));
+        $end = intval($request->get('end'));
+        if($start > $end)
+        {
+            $start = intval($request->get('end'));
+            $end = intval($request->get('start'));
+        }
+        
         $em = $this->getDoctrine()->getEntityManager();
         
-        $targetUser = $em->createQuery('SELECT u FROM ColectaUserBundle:User u WHERE u.name = :uname')->setParameter('uname',$request->get('user'))->getSingleResult();
-        if(!$targetUser)
+        $targetUser = $em->createQuery('SELECT u FROM ColectaUserBundle:User u WHERE u.name = :uname')->setParameter('uname',$request->get('user'))->getResult();
+        if(!count($targetUser))
         {
             $this->get('session')->setFlash('error', 'No se ha encontrado al usuario.');
             
@@ -71,13 +86,15 @@ class LotteryController extends Controller
             return new RedirectResponse($login);
         }
         
+        $targetUser = $targetUser[0];
+                
         $campaign = $em->createQuery('SELECT c FROM ColectaIntranetBundle:LotteryCampaign c ORDER BY c.date DESC')->getSingleResult();
         
-        $shred = $em->createQuery('SELECT s FROM ColectaIntranetBundle:LotteryShred s WHERE s.lotteryCampaign = :campaign AND s.start <= :end AND s.end >= :start ORDER BY s.date ASC')->setParameters(array('campaign'=>$campaign, 'start'=>$request->get('start'), 'end'=>$request->get('end')))->getResult();
+        $shred = $em->createQuery('SELECT s FROM ColectaIntranetBundle:LotteryShred s WHERE s.lotteryCampaign = :campaign AND s.start <= :end AND s.end >= :start ORDER BY s.date ASC')->setParameters(array('campaign'=>$campaign, 'start'=>$start, 'end'=>$end))->getResult();
         
         if(count($shred))
         {
-            if($this->checkRepeatedShred($em, $shred[0], $request->get('start'), $request->get('end')))
+            if($this->checkRepeatedShred($em, $shred[0], $start, $end))
             {
                 $this->get('session')->setFlash('error', 'La selección es incompatible');
                 
@@ -88,8 +105,8 @@ class LotteryController extends Controller
                 
         $newShred = new LotteryShred();
         $newShred->setUser($targetUser);
-        $newShred->setStart($request->get('start'));
-        $newShred->setEnd($request->get('end'));
+        $newShred->setStart($start);
+        $newShred->setEnd($end);
         $newShred->setDate(new \DateTime('now'));
         if($request->get('paid') == 'on')
         {
@@ -113,12 +130,15 @@ class LotteryController extends Controller
     
     public function checkRepeatedShred($em, $shred, $start, $end)
     {
+        /* Search for other lottery shred contained between start and end */
         $compareShred = $em->createQuery('SELECT s FROM ColectaIntranetBundle:LotteryShred s WHERE s.lotteryCampaign = :campaign AND s.start <= :end AND s.end >= :start AND s.date > :date ORDER BY s.date ASC')->setParameters(array('campaign'=>$shred->getLotteryCampaign(), 'start'=>$start, 'end'=>$end, 'date'=>$shred->getDate()))->getResult();
         
+        /* if previous found shred is a returned shred and there are no more shreds on those numbers, it is ok to go */
         if($shred->getReturned() and !count($compareShred)) 
         {
             return false;
         }
+        /* if previous found shred is not returned and there are no more shreds on those numbers, this is a repeated request */
         elseif(!count($compareShred))
         {
             return true;
@@ -126,17 +146,21 @@ class LotteryController extends Controller
         
         $compareShred = $compareShred[0];
         
+        /* if this lottery shred is a returned one… */
         if($compareShred->getReturned())
         {
+            /* if totally contains the start to end shred, keep checking further history, ok for now…  */
             if($compareShred->getStart() <= $start and $compareShred->getEnd() >= $end)
             {
                 return $this->checkRepeatedShred($em, $compareShred, $start, $end);
             }
+            /* if it is only a partial return, fail */
             else
             {
                 return true;
             }
         }
+        /* if it is not a returned one, fail */
         else
         {
             return true;
@@ -183,7 +207,6 @@ class LotteryController extends Controller
     public function returnShredAction($shred_id)
     {
         $user = $this->get('security.context')->getToken()->getUser();
-        $request = $this->get('request')->request;
         
         if($user == 'anon.' || !$user->getRole()->getSiteConfig())
         {
@@ -191,20 +214,70 @@ class LotteryController extends Controller
             return new RedirectResponse($login);
         }
         
+        $request = $this->get('request')->request;
+        
+        $start = intval($request->get('start'));
+        $end = intval($request->get('end'));
+        if($start > $end)
+        {
+            $start = intval($request->get('end'));
+            $end = intval($request->get('start'));
+        }
+        
         $em = $this->getDoctrine()->getEntityManager();
         
-        $shred = $em->createQuery('SELECT s FROM ColectaIntranetBundle:LotteryShred s WHERE s.id = :id AND s.returned = 0 ORDER BY s.date ASC')->setParameter('id',$shred_id)->getResult();
+        $shred = $em->createQuery('SELECT s FROM ColectaIntranetBundle:LotteryShred s WHERE s.id = :id AND s.returned = 0 ORDER BY s.date DESC')->setParameter('id',$shred_id)->getResult();
         
-        if(!count($shred))
+        /* The lottery shred does not exist on database or is already a returned shred */
+        if(!count($shred) || $shred[0]->getReturned())
         {
             $this->get('session')->setFlash('error', 'No se ha encontrado la referencia.');
             
-            $login = $this->generateUrl('ColectaBackendLotteryIndex');
-            return new RedirectResponse($login);
+            $referer = $this->get('request')->headers->get('referer');
+        
+            if(empty($referer))
+            {
+                $referer = $this->generateUrl('ColectaBackendLotteryIndex');
+            }
+            
+            return new RedirectResponse($referer);
         }
         
         $shred = $shred[0];
         
+        /* Check if there has been a return of this shred matching start and end */
+        $returnedShred = $em->createQuery('SELECT s FROM ColectaIntranetBundle:LotteryShred s WHERE s.id > :id AND s.returned = 1 AND (s.start <= :end AND s.end >= :start) ORDER BY s.date DESC')->setParameters(array('id'=>$shred_id, 'start'=>$start, 'end'=>$end))->getResult();
+        
+        if(count($returnedShred))
+        {
+            $this->get('session')->setFlash('error', 'No puede realizarse la devolución porque coincide con otra anterior.');
+            
+            $referer = $this->get('request')->headers->get('referer');
+        
+            if(empty($referer))
+            {
+                $referer = $this->generateUrl('ColectaBackendLotteryIndex');
+            }
+            
+            return new RedirectResponse($referer);
+        }
+        
+        /* The return is out of bounds of original shred */
+        if(intval($request->get('start')) < $shred->getStart() || intval($request->get('end')) > $shred->getEnd())
+        {
+            $this->get('session')->setFlash('error', 'Devolución fuera de rango.');
+            
+            $referer = $this->get('request')->headers->get('referer');
+        
+            if(empty($referer))
+            {
+                $referer = $this->generateUrl('ColectaBackendLotteryIndex');
+            }
+            
+            return new RedirectResponse($referer);
+        }
+        
+        /* Ok, create new shred */
         $newShred = new LotteryShred();
         $newShred->setUser($shred->getUser());
         $newShred->setStart($request->get('start'));
@@ -218,6 +291,102 @@ class LotteryController extends Controller
         $em->flush();
         
         $this->get('session')->setFlash('success', 'Devolución realizada.');
+        
+        $referer = $this->get('request')->headers->get('referer');
+        
+        if(empty($referer))
+        {
+            $referer = $this->generateUrl('ColectaBackendLotteryIndex');
+        }
+        
+        return new RedirectResponse($referer);
+    }
+    
+    public function unpaidShredAction($shred_id)
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        
+        if($user == 'anon.' || !$user->getRole()->getSiteConfig())
+        {
+            $login = $this->generateUrl('userLogin');
+            return new RedirectResponse($login);
+        }
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $shred = $em->createQuery('SELECT s FROM ColectaIntranetBundle:LotteryShred s WHERE s.id = :id AND s.returned = 0 ORDER BY s.date DESC')->setParameter('id',$shred_id)->getResult();
+        
+        /* The lottery shred does not exist on database or is already a returned shred */
+        if(!count($shred) || !$shred[0]->getPaid())
+        {
+            $this->get('session')->setFlash('error', 'No se ha encontrado la referencia.');
+            
+            $referer = $this->get('request')->headers->get('referer');
+        
+            if(empty($referer))
+            {
+                $referer = $this->generateUrl('ColectaBackendLotteryIndex');
+            }
+            
+            return new RedirectResponse($referer);
+        }
+        
+        $shred = $shred[0];
+        
+        $shred->setPaid(0);
+        
+        $em->persist($shred); 
+        $em->flush();
+        
+        $this->get('session')->setFlash('success', 'Marcado correctamente como no pagado.');
+        
+        $referer = $this->get('request')->headers->get('referer');
+        
+        if(empty($referer))
+        {
+            $referer = $this->generateUrl('ColectaBackendLotteryIndex');
+        }
+        
+        return new RedirectResponse($referer);
+    }
+    
+    public function paidShredAction($shred_id)
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        
+        if($user == 'anon.' || !$user->getRole()->getSiteConfig())
+        {
+            $login = $this->generateUrl('userLogin');
+            return new RedirectResponse($login);
+        }
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $shred = $em->createQuery('SELECT s FROM ColectaIntranetBundle:LotteryShred s WHERE s.id = :id AND s.returned = 0 ORDER BY s.date DESC')->setParameter('id',$shred_id)->getResult();
+        
+        /* The lottery shred does not exist on database or is already a returned shred */
+        if(!count($shred) || $shred[0]->getPaid())
+        {
+            $this->get('session')->setFlash('error', 'No se ha encontrado la referencia.');
+            
+            $referer = $this->get('request')->headers->get('referer');
+        
+            if(empty($referer))
+            {
+                $referer = $this->generateUrl('ColectaBackendLotteryIndex');
+            }
+            
+            return new RedirectResponse($referer);
+        }
+        
+        $shred = $shred[0];
+        
+        $shred->setPaid(1);
+        
+        $em->persist($shred); 
+        $em->flush();
+        
+        $this->get('session')->setFlash('success', 'Marcado correctamente como no pagado.');
         
         $referer = $this->get('request')->headers->get('referer');
         
