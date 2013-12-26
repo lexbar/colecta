74fbc5cb876b1da7efd90f0c2916d6ceb0b73c61
Alex <zumbenet@gmail.com>
2013-12-26 18:43:35 +0100

    Bug fix

--- src/Colecta/BackendBundle/Controller/UserController.php
+++ src/Colecta/BackendBundle/Controller/UserController.php
@@ -66,7 +66,7 @@ class UserController extends Controller
                 }
                 else
                 {
-                    $profile->setPartnerId(empty($request->get('partnerId')) ? null : $request->get('partnerId'));
+                    $profile->setPartnerId($request->get('partnerId'));
                 }
             }
             
