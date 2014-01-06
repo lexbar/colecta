66f0863e33b184575328e27f9aa193e41c3f0f99
Alex <zumbenet@gmail.com>
2014-01-06 13:55:30 +0100

    Points applied to rank

--- src/Colecta/IntranetBundle/Controller/ActivitiesController.php
+++ src/Colecta/IntranetBundle/Controller/ActivitiesController.php
@@ -42,10 +42,10 @@ class ActivitiesController extends Controller
     
     public function rankAction()
     {
-        return $this->yearRankAction(date('Y'));
+        return $this->yearPointsRankAction(date('Y'));
     }
     
-    public function yearRankAction($year)
+    public function yearKmRankAction($year)
     {
         $em = $this->getDoctrine()->getEntityManager();
         $user = $this->get('security.context')->getToken()->getUser();
@@ -106,5 +106,85 @@ class ActivitiesController extends Controller
         $years = $stmt->fetchAll();
         
         return $this->render('ColectaIntranetBundle:Activities:rank.html.twig', array('users'=>$users, 'kms'=>$kms, 'year'=>$year, 'years'=>$years));
+    }
+    public function yearPointsRankAction($year)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $user = $this->get('security.context')->getToken()->getUser();
+        
+        if($user == 'anon.')
+        {
+            $login = $this->generateUrl('userLogin');
+            return new RedirectResponse($login);
+        }
+        
+        $year = min( intval(date('Y')), max( 1990, intval($year) ) );
+        
+        $users = $em->getRepository('ColectaUserBundle:User')->findAll();
+        
+        $points = array(1=>'',2=>'');
+        $kms = array(1=>'',2=>'');
+        
+        if(count($users))
+        {
+            $newusers = array(); //the new array of users, that will be sorted by total kms
+            foreach($users as $u)
+            {
+                if($u->getRole() && $u->getRole()->getName() != 'ROLE_BANNED')
+                {
+                    $p = 0;
+                
+                    $pointsRequest = $em->createQuery('SELECT p FROM ColectaUserBundle:Points p WHERE p.user = :user AND p.date >= \''.$year.'-01-01 00:00:00\' AND p.date <= \''.$year.'-12-31 23:59:59\'')->setParameter('user', $u)->getResult();
+                    
+                    if(count($pointsRequest))
+                    {
+                        foreach($pointsRequest as $point)
+                        {
+                            $p += $point->getPoints();
+                        }
+                    }
+                    
+                    $points[$u->getId()] = $p;
+                    
+                    $k = 0;
+                
+                    $assistances = $em->createQuery('SELECT a FROM ColectaActivityBundle:Event e, ColectaActivityBundle:EventAssistance a WHERE a.event = e AND a.user = :user AND a.confirmed = 1 AND e.dateend >= \''.$year.'-01-01 00:00:00\' AND e.dateini <= \''.$year.'-12-31 23:59:59\' ORDER BY e.dateini ASC')->setParameter('user', $u)->getResult();
+                    
+                    if(count($assistances))
+                    {
+                        foreach($assistances as $as)
+                        {
+                            $k += $as->getKm();
+                        }
+                    }
+                    
+                    $kms[$u->getId()] = $k;
+                    
+                    $newusers[$p][] = $u;
+                }
+            }
+            
+            // now we resort the array to make it of 1d instead of 2d
+            $newusers2 = array();
+            krsort($newusers);
+            foreach($newusers as $nu)
+            {
+                foreach($nu as $nu2)
+                {
+                    $newusers2[] = $nu2;
+                }
+            }
+            $users = $newusers2;
+        }
+        
+        //Get the years with assistances
+        $stmt = $em  
+               ->getConnection()  
+               ->prepare('SELECT DISTINCT(YEAR(e.dateini)) AS year FROM Event e WHERE  e.dateend <= \''.date('Y').'-12-31 00:00:00\' ORDER BY e.dateini ASC');
+               
+        $stmt->execute();  
+        $years = $stmt->fetchAll();
+        
+        return $this->render('ColectaIntranetBundle:Activities:pointsrank.html.twig', array('users'=>$users, 'points'=>$points, 'kms'=>$kms, 'year'=>$year, 'years'=>$years));
     }
 }
