608d09013ecc753ea71d383e1e3b948ec81ee019
Alex <zumbenet@gmail.com>
2014-01-07 20:41:46 +0100

    Backend user creation, bug fixes and design improvements on user account

--- src/Colecta/BackendBundle/Controller/UserController.php
+++ src/Colecta/BackendBundle/Controller/UserController.php
@@ -3,6 +3,9 @@
 namespace Colecta\BackendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Colecta\UserBundle\Entity\User;
+use Colecta\UserBundle\Entity\UserProfile;
 
 class UserController extends Controller
 {
@@ -97,7 +100,155 @@ class UserController extends Controller
         
         return $this->render('ColectaBackendBundle:User:profile.html.twig', array('user'=>$user, 'roles'=>$roles));
     }
-    public function activityReportAction($format)
+    public function newUserAction()
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        if($user == 'anon.' || !$user->getRole()->getSiteConfigUsers())
+        {
+            return new RedirectResponse($this->generateUrl('ColectaDashboard'));
+        }
+        
+        $user = new User();
+        
+        if ($this->get('request')->getMethod() == 'POST') 
+        {
+            $request = $this->get('request')->request;
+            
+            if($request->get('userName'))
+            {
+                $user->setName($request->get('userName'));
+            }
+            else
+            {
+                $this->get('session')->setFlash('error', 'Debes escribir un nombre de usuario.');
+            }
+            
+            if($request->get('userMail'))
+            {
+                $user->setMail($request->get('userMail'));
+            }
+            else
+            {
+                $this->get('session')->setFlash('error', 'Debes escribir un correo electrónico.');
+            }
+            
+            $role = $em->getRepository('ColectaUserBundle:Role')->findOneById($request->get('userRole'));
+            $user->setRole($role);
+            
+            $profile = new UserProfile();
+            $user->setProfile($profile);
+            
+            $profile->setName($request->get('name'));
+            $profile->setSurname($request->get('surname'));
+            $profile->setSex($request->get('sex'));
+            
+            if($request->get('birthDateYear') && $request->get('birthDateMonth') && $request->get('birthDateDay'))
+            {
+                $year = intval($request->get('birthDateYear'));
+                $month = intval($request->get('birthDateMonth'));
+                $day = intval($request->get('birthDateDay'));
+                
+                if(1900 < $year  && $year <= intval(date('Y')) && 1 <= $month && $month <= 12 && 1 <= $day && $day <= 31)
+                {
+                    $birthDateTime = new \DateTime($year.'-'.$month.'-'.$day.' 00:00:00');
+                    $profile->setBirthDate($birthDateTime);
+                }
+                else
+                {
+                    $this->get('session')->setFlash('error', 'La fecha de nacimiento '."".' no tiene un valor aceptable.');
+                }
+            }
+            
+            $profile->setAddress($request->get('address'));
+            $profile->setPhone($request->get('phone'));
+            $profile->setIdNumber($request->get('idNumber'));
+            
+            if($request->get('partnerId'))
+            {
+                $partnerIdUser = $em->getRepository('ColectaUserBundle:UserProfile')->findOneByPartnerId($request->get('partnerId'));
+                
+                if($partnerIdUser && $partnerIdUser->getUser() != $user)
+                {
+                    $this->get('session')->setFlash('error', 'El número de socio no se ha establecido porque pertenece a '.$partnerIdUser->getUser()->getName());
+                }
+                else
+                {
+                    $profile->setPartnerId($request->get('partnerId'));
+                }
+            }
+            else
+            {
+                $profile->setPartnerId(null);
+            }
+            
+            $profile->setComments($request->get('comments'));
+            
+            //Welcome Text
+            $welcomeText = $request->get('welcomeText');
+            
+            if($user->getName() && $user->getMail())
+            {
+                $user->setPass('');
+                $user->setAvatar('');
+                $user->setRegistered(new \DateTime('now'));
+                $user->setLastAccess(new \DateTime('now'));
+                
+                $salt = md5(time());
+                $user->setSalt($salt);
+                
+                $code = substr(md5($salt.$this->container->getParameter('secret')),5,18);
+                
+                $em->persist($user);
+                $em->persist($profile); 
+                $em->flush();
+                $this->get('session')->setFlash('success', 'Usuario '. $user->getName() .' creado correctamente');
+                
+                if($request->get('notificateUser') == 'on')
+                {
+                    $mailer = $this->get('mailer');
+                    
+                    //Get the mail address the message is sent from
+                    $configmail = $this->container->getParameter('mail');
+                    
+                    //Get this web title
+                    $twig = $this->container->get('twig');
+                    $globals = $twig->getGlobals();
+                    $webTitle = $globals['web_title'];
+                    
+                    //Welcome Text
+                    $welcomeText = str_replace(array('%N', '%L'), array($user->getName(), $this->get('router')->generate('userResetPasswordCode', array('uid'=>$user->getId(), 'code'=>$code), true)), $welcomeText);
+                    
+                    $message = \Swift_Message::newInstance();
+                    //$logo = $message->embed(\Swift_Image::fromPath($this->get('kernel')->getRootDir().'/../web/logo.png'));
+    			    $message->setSubject('Cuenta creada en '.$webTitle)
+    			        ->setFrom($configmail['from'])
+    			        ->setTo($user->getMail())
+    			        //->addPart($this->renderView('::foo.txt.twig', array(), 'text/plain')
+    			        ->setBody($welcomeText);
+    			    $mailer->send($message);
+                }
+                
+                $this->get('request')->SetMethod("GET"); // Set request method to Get before redirection 
+                return new RedirectResponse($this->generateUrl('ColectaBackendUserProfile', array('user_id'=>$user->getId()))); //User profile edit page
+            }
+        }
+        else
+        {
+            //Get this web title
+            $twig = $this->container->get('twig');
+            $globals = $twig->getGlobals();
+            $webTitle = $globals['web_title']; 
+                    
+            $welcomeText = str_replace('---web_title---', $webTitle, $this->container->getParameter('welcomeText'));
+        }
+        
+        $roles = $em->getRepository('ColectaUserBundle:Role')->findAll();
+        
+        return $this->render('ColectaBackendBundle:User:newUser.html.twig', array('user'=>$user, 'roles'=>$roles, 'welcomeText'=>$welcomeText));
+    }
+    public function activityReportAction($format) //Special report for Turyciclo
     {
         $year = 2013;
         
