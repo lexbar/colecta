700598e5ae5502bc248be64a46456ee8826e4ea5
lexbar <zumbenet@gmail.com>
2015-08-22 21:30:11 +0200

    Improved private messages

--- src/Colecta/UserBundle/Controller/MessageController.php
+++ src/Colecta/UserBundle/Controller/MessageController.php
@@ -5,6 +5,7 @@ namespace Colecta\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\SecurityContext;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use Colecta\UserBundle\Entity\Notification;
 use Colecta\UserBundle\Entity\Message;
 
@@ -24,7 +25,42 @@ class MessageController extends Controller
         
         $em = $this->getDoctrine()->getManager();
         
-        $messages = $em->getRepository('ColectaUserBundle:Message')->findBy(array('destination'=>$user->getId()), array('date'=>'DESC'),30,0);
+        
+        $sql = "SELECT m.id, m.responseto_id, m.user_origin_id, m.date, m.text, m.dismiss, u.id AS destination_id, u.name, u.role_id, u.profile_id, u.mail, u.avatar " .
+        "FROM Message m INNER JOIN User u ON m.user_destination_id = u.id " .
+        "WHERE m.id IN " .
+        "(SELECT MAX(m2.id) as maxid FROM Message m2 WHERE m.user_origin_id = :user_id OR m.user_destination_id = :user_id GROUP BY LEAST(m2.user_origin_id,m2.user_destination_id), GREATEST(m2.user_origin_id,m2.user_destination_id)) " .
+        "ORDER BY m.date DESC ";
+       
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('ColectaUserBundle:Message', 'm');
+        $rsm->addJoinedEntityFromClassMetadata('ColectaUserBundle:User', 'u', 'm', 'destination', array('id' => 'destination_id'));
+        
+        $query = $em->createNativeQuery($sql, $rsm);
+        $query->setParameter('user_id', $user->getId());
+        
+        $messages = $query->getResult();
+        
+        return $this->render('ColectaUserBundle:Message:index.html.twig', array('messages' => $messages));
+    }
+    
+    public function conversationAction($user_id)
+    {
+        $user = $this->getUser();
+        
+        if(!$user)
+        {
+            $this->get('session')->getFlashBag()->add('error', 'Error, debes iniciar sesion');
+            return new RedirectResponse($this->generateUrl('userLogin'));
+        }
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        $interlocutor = $em->getRepository('ColectaUserBundle:User')->findOneById($user_id);
+        
+        $messages = $em->createQuery(
+            "SELECT m FROM ColectaUserBundle:Message m WHERE m.origin = :user_id OR m.destination = :user_id ORDER BY m.date DESC"
+        )->setParameter('user_id', $user_id)->getResult();
         
         if(count($messages))
         {
@@ -42,7 +78,7 @@ class MessageController extends Controller
             }
         }
         
-        return $this->render('ColectaUserBundle:Message:index.html.twig', array('messages' => $messages));
+        return $this->render('ColectaUserBundle:Message:conversation.html.twig', array('messages' => $messages, 'interlocutor' => $interlocutor));
     }
     
     public function sentAction()
@@ -108,33 +144,39 @@ class MessageController extends Controller
             
             if($persist)
             {
-                $message = new Message();
+                try
+                {
+                    $message = new Message();
                 
-                $message->setDate(new \DateTime('now'));
-                $message->setText($text);
-                $message->setResponseto(null);
-                $message->setOrigin($user);
-                $message->setDestination($destinationUser);
-                $message->setDismiss(false);
+                    $message->setDate(new \DateTime('now'));
+                    $message->setText($text);
+                    $message->setResponseto(null);
+                    $message->setOrigin($user);
+                    $message->setDestination($destinationUser);
+                    $message->setDismiss(false);
+                    
+                    $em->persist($message); 
+                    $em->flush();
+                    
+                    //Send mail notification
+                    
+                    $mailer = $this->get('mailer');
+                    $configmail = $this->container->getParameter('mail');
+                    
+                    $message = \Swift_Message::newInstance();
+    			    $message->setSubject('Mensaje de '.$user->getName())
+    			        ->setFrom($configmail['from'])
+    			        ->setTo($destinationUser->getMail())
+    			        ->setBody($this->renderView('ColectaUserBundle:Message:mailAlert.txt.twig', array('from'=>$user, 'to'=>$destinationUser)), 'text/plain');
+    			    $mailer->send($message);
+                    
+                    $this->get('session')->getFlashBag()->add('success', 'Mensaje enviado.');
+                }
+                catch (\Exception $e) {
+                    $this->get('session')->getFlashBag()->add('error', 'No se ha podido notificar por email al receptor, pero el mensaje ha sido correctamente almacenado.');
+                }
                 
-                $em->persist($message); 
-                $em->flush();
-                
-                //Send mail notification
-                
-                $mailer = $this->get('mailer');
-                $configmail = $this->container->getParameter('mail');
-                
-                $message = \Swift_Message::newInstance();
-			    $message->setSubject('Mensaje de '.$user->getName())
-			        ->setFrom($configmail['from'])
-			        ->setTo($destinationUser->getMail())
-			        ->setBody($this->renderView('ColectaUserBundle:Message:mailAlert.txt.twig', array('from'=>$user, 'to'=>$destinationUser)), 'text/plain');
-			    $mailer->send($message);
-                
-                $this->get('session')->getFlashBag()->add('success', 'Mensaje enviado.');
-                
-                return new RedirectResponse($this->generateUrl('userSentMessages'));
+                return new RedirectResponse($this->generateUrl('userConversation', array('user_id' => $destinationUser->getId())));
             }
             else
             {
